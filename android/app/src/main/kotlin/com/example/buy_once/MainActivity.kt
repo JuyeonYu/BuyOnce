a97efe54178569655549422a19ab836ca0e4feab
juyeon <remake867@gmail.com>
2024-03-20 23:11:45 +0900

    init

---
+++ android/app/src/main/kotlin/com/example/buy_once/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.buy_once
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:buy_once/splash_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const _App());
+}
+
+class _App extends StatelessWidget {
+  const _App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    );
+  }
+}
